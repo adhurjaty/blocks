d6eba91dba2c817e55b45970733bbb02fd1548ba
Anil Dhurjaty <adhurjaty@gmail.com>
2022-12-03 15:45:18 -0800

    implement score checking

--- Blocks/Game.cs
+++ Blocks/Game.cs
@@ -62,9 +62,17 @@ public class Game
         return new BlockManipulator(block);
     }
 
-    public bool PlaceBlock(BlockOrientation block)
+    public bool PlaceBlock(BlockOrientation block, Vec3<int> pos)
     {
-        
+        if (!CanPlaceBlock(block, pos)) return false;
+
+        var (first, second) = block.GetColorPositions(pos);
+        foreach(var cube in new[] { first, second })
+        {
+            var cubePos = cube.Position;
+            _state.Board[cubePos.X, cubePos.Y, cubePos.Z] = cube.Color.ToSpace();
+        }
+        return true;
     }
 
     public bool CanPlaceBlock(BlockOrientation block, Vec3<int> pos)
@@ -84,10 +92,40 @@ public class Game
             };
     }
 
-    // public Score GetScore()
-    // {
+    public Score GetScore()
+    {
+        var score = GetVolumeSpaces(
+            new[] { 0, _state.Board.GetLength(0) - 1 },
+            Enumerable.Range(0, _state.Board.GetLength(1)),
+            Enumerable.Range(0, _state.Board.GetLength(2)))
+            .Concat(GetVolumeSpaces(
+                Enumerable.Range(0, _state.Board.GetLength(0)),
+                Enumerable.Range(0, _state.Board.GetLength(1)),
+                new[] { 0, _state.Board.GetLength(2) - 1 }
+            ))
+            .Concat(GetVolumeSpaces(
+                Enumerable.Range(0, _state.Board.GetLength(0)),
+                new[] { _state.Board.GetLength(1) - 1 },
+                Enumerable.Range(0, _state.Board.GetLength(2))
+            )).GroupBy(x => x)
+            .ToDictionary(x => x.Key, x => x.Count());
+        return new Score
+        (
+            Black: score.GetValueOrDefault(Space.BLACK, 0),
+            White: score.GetValueOrDefault(Space.WHITE, 0)
+        );
+    }
 
-    // }
+    private IEnumerable<Space> GetVolumeSpaces(
+        IEnumerable<int> xs,
+        IEnumerable<int> ys,
+        IEnumerable<int> zs
+    )
+    {
+        return xs.SelectMany(x =>
+            ys.SelectMany(y =>
+                zs.Select(z => _state.Board[x, y, z])));
+    }
 }
 
 public record Score
